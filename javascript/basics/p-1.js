ff54558074e8bf154f5271001b471e9e8ee622ea
Krutik Aghera <agherakrutik99@gmail.com>
2022-11-25 22:20:27 +0530

    is prime number or not

--- javascript/basics/p-1.js
+++ javascript/basics/p-1.js
@@ -1,3 +1,16 @@
-var k = 0;
-k = 7;
+let count = 0;
 
+let number = 83;
+for(let i = 2;i <= number/2;i++)
+{
+    if(number % i == 0)
+        count++;
+}
+
+if(count > 0){
+    console.log("number "+number+" is not a prime number.");
+}
+else
+{
+    console.log("number " + number + " is a prime number");
+}
