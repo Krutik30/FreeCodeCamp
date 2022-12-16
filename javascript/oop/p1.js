001c74c153f7a1747677c9c28ab938ecbae48762
Krutik Aghera <agherakrutik99@gmail.com>
2022-12-16 22:29:24 +0530

    oop day-3

--- javascript/oop/p1.js
+++ javascript/oop/p1.js
@@ -118,3 +118,76 @@ Dog.prototype = {
     console.log("My name is "+this.name+" BHAv?");
   }
 };
+
+
+function Dog(name) {
+  this.name = name;
+}
+Dog.prototype = {
+  constructor: Dog,
+  numLegs: 4,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
+
+function Dog(name) {
+  this.name = name;
+}
+beagle = new Dog("Snoopy");
+Dog.prototype.isPrototypeOf(beagle);
+
+function Dog(name) {
+  this.name = name;
+}
+
+beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
+
+
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Animal() { 
+
+}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+Cat.prototype = {
+  constructor: Cat
+}
+Bear.prototype = {
+  constructor: Bear
+}
