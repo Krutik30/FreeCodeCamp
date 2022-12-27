4633c857f781c20a89570ea0ad2a3566aecd408c
Krutik Aghera <agherakrutik99@gmail.com>
2022-12-27 10:02:13 +0530

    day - 2 Inter algo script

--- javascript/Intermidiate_algo_script/p1.js
+++ javascript/Intermidiate_algo_script/p1.js
@@ -59,3 +59,66 @@ function sumAll(arr) {
     inner(arr2,arr1);
   */
   diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+  function destroyer(arr) {
+    const valsToRemove = Object.values(arguments).slice(1);
+    // pehla vala array hai uske siva kitne hai 
+    // kya hai use store kare bina nai jan skte
+    // to hamare pass argument object hai jo hame help karta hai 
+    // object.value(arguments) - yaha [0] do to first
+    // ese array ki tarah kam karta hai 
+    // sabhi dedi arg to ab hame array ke bad vali access karni hai
+    // to slice karke array bana liya
+    const filteredArray = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      let removeElement = false;
+      // initialize kiya ke value nai hai
+      for (let j = 0; j < valsToRemove.length; j++) {
+        if (arr[i] === valsToRemove[j]) {
+          removeElement = true;
+          // fir check kiya ke kahi hamne banaye array me hai to nai
+          // hoga to true bol diya
+        }
+      }
+      if (!removeElement) {
+        filteredArray.push(arr[i]);
+        // jo nai hai fir to hamara jo array ban raha hai usme store karlo
+      }
+    }
+    return filteredArray;
+    // or return to karna hi hai;
+  }
+
+  function whatIsInAName(collection, source) {
+
+    const souceKeys = Object.keys(source);
+    console.log(souceKeys);
+    // ye .keys() hame source me diye hue keys return karke deta hai
+    // ham ise collect karke check karenge
+    // filter the collection
+    return collection.filter(obj => {
+      // main return karenge kon kon ye keys rakhta hai
+      for (let i = 0; i < souceKeys.length; i++) {
+        // loop dalke sabhi check karge na
+        if (!obj.hasOwnProperty(souceKeys[i]) 
+        // ye bolta hai ke hamri collected key collection me nai hai
+        || obj[souceKeys[i]] !== source[souceKeys[i]])
+        // or uski value same nai hai fir wo false karega return
+         {
+          return false;
+        }
+      }
+       // agar dono match ho jaye fir true return karega
+      return true;
+    });
+    // ye call back function tha true return karega
+    // tabhi filter fucntion filter karega kisko rakhna
+    // kisko nai rakhna 
+  
+  }
+  
+  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+  
+  // source vala object jis colllection me ho 
+  // use return karna hai
