f18ef14abb6ff2caf7c7ffa1535403916ddb9f61
Krutik Aghera <agherakrutik99@gmail.com>
2023-01-17 22:57:31 +0530

    completed react and redux

--- FE_library/react and redux.js
+++ FE_library/react and redux.js
@@ -127,3 +127,90 @@ class AppWrapper extends React.Component {
 
   // Change code above this line
 };
+
+
+const state = [];
+
+// change code below this line
+const mapStateToProps = (state)=>{
+  return {
+    messages: state
+  }
+}
+
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+
+// Change code below this line
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+      submitNewMessage: function(message) {
+      dispatch(addMessage(message));
+    }
+  }
+}
+
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {
+    messages: state
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (message) => {
+      dispatch(addMessage(message));
+    }
+  }
+};
+
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h3>This is a Presentational Component</h3>
+  }
+};
+
+const connect = ReactRedux.connect;
+// Change code below this line
+const ConnectedComponent = connect(mapStateToProps,mapDispatchToProps)(Presentational);
+
+/*
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider, connect } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+import rootReducer from './redux/reducers'
+import App from './components/App'
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
+*/
+
+// Only change code below this line
+console.log('Now I know React and Redux!');
